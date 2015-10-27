2769f738c1a12fcfb72c2fb8d9c7a6bc31f4c8c4
loic <lgevrey@oxya.com>
2015-10-27 19:05:02 +0100

    UI imrpvoement

--- desktop/php/netatmoWelcome.php
+++ desktop/php/netatmoWelcome.php
@@ -49,36 +49,42 @@ foreach ($eqLogics as $eqLogic) {
 ?>
 </div>
 <div class="col-sm-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-    <form class="form-horizontal">
-     <fieldset>
-      <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-      <div class="form-group">
-        <label class="col-sm-4 control-label">{{Nom de l'équipement Welcome Netatmo}}</label>
-        <div class="col-sm-6">
-          <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-          <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement météo Netatmo}}"/>
+<legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+  <div class="row">
+    <div class="col-sm-6">
+      <form class="form-horizontal">
+       <fieldset>
+        <div class="form-group">
+          <label class="col-sm-4 control-label">{{Nom de l'équipement Welcome Netatmo}}</label>
+          <div class="col-sm-6">
+            <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+            <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement météo Netatmo}}"/>
+          </div>
         </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-4 control-label" >{{Objet parent}}</label>
-        <div class="col-sm-6">
-          <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-            <option value="">{{Aucun}}</option>
-            <?php
+        <div class="form-group">
+          <label class="col-sm-4 control-label" >{{Objet parent}}</label>
+          <div class="col-sm-6">
+            <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+              <option value="">{{Aucun}}</option>
+              <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-         </select>
+           </select>
+         </div>
        </div>
-     </div>
-     <div class="form-group">
-       <label class="col-sm-4 control-label"></label>
-       <div class="col-sm-8">
-         <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-         <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+       <div class="form-group">
+         <label class="col-sm-4 control-label"></label>
+         <div class="col-sm-8">
+           <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
+           <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+         </div>
        </div>
-     </div>
+     </fieldset>
+   </form>
+ </div>
+</div>
 
 <legend>{{Commandes}}</legend>
 <table id="table_cmd" class="table table-bordered table-condensed">
