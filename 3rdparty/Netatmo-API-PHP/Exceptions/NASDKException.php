beed6cee9e228452994c1a13be5f6cce1ebb9254
Loïc <loic@jeedom.com>
2018-10-30 11:50:06 +0100

    test

--- 3rdparty/Netatmo-API-PHP/Exceptions/NASDKException.php
+++ 3rdparty/Netatmo-API-PHP/Exceptions/NASDKException.php
@@ -1,17 +1,21 @@
 <?php
 /**
-* Wrapper ensuring backward compatibility with older SDK versions
-*
-* DEPRECATED
-*
-* You must not use this class
-*
+* Exception thrown by Netatmo SDK
 */
-
-require_once dirname(dirname(__FILE__)) . "/Netatmo/autoload.php";
-
-class NASDKException extends Netatmo\Exceptions\NASDKException
+class NASDKException extends Exception
 {
+    public function __construct($code, $message)
+    {
+        parent::__construct($message, $code);
+    }
 }
 
+
+class NASDKError
+{
+    const UNABLE_TO_CAST = 601;
+    const NOT_FOUND = 602;
+    const INVALID_FIELD = 603;
+    const FORBIDDEN_OPERATION = 604;
+}
 ?>
--- 3rdparty/Netatmo-API-PHP/Objects/NAPerson.php
+++ 3rdparty/Netatmo-API-PHP/Objects/NAPerson.php
@@ -1,17 +1,64 @@
 <?php
+
+require_once('NAObject.php');
+
 /**
-* Wrapper ensuring backward compatibility with older SDK versions
-*
-* DEPRECATED
-*
-* You must not use this class
-*
+* class NAPerson
 */
-
-require_once dirname(dirname(__FILE__)) . "/Netatmo/autoload.php";
-
-class NAPerson extends Netatmo\Objects\NAPerson
+class NAPerson extends NAObjectWithPicture
 {
-}
+    /**
+    * @return bool
+    * @brief returns whether or not this person is known
+    */
+    public function isKnown()
+    {
+        if($this->getVar(NACameraPersonInfo::CPI_PSEUDO, FALSE))
+            return TRUE;
+        else return FALSE;
+    }
 
+    /**
+    * @return bool
+    * @brief returns whether or not this person is unknown
+    */
+
+    public function isUnknown()
+    {
+        return !$this->isKnown();
+    }
+
+    /**
+    * @return bool
+    * @brief returns whether or not this person is at home
+    */
+    public function isAway()
+    {
+        return $this->getVar(NACameraPersonInfo::CPI_OUT_OF_SIGHT);
+    }
+
+    public function getFace()
+    {
+        $face = $this->getVar(NACameraPersonInfo::CPI_FACE);
+        return $this->getPictureURL($face);
+    }
+
+    /**
+    * @return timestamp
+    * @brief returns last time this person has been seen
+    */
+    public function getLastSeen()
+    {
+        return $this->getVar(NACameraPersonInfo::CPI_LAST_SEEN);
+    }
+
+    /**
+    * @return string
+    * @brief returns this person's name
+    */
+    public function getPseudo()
+    {
+        return $this->getVar(NACameraPersonInfo::CPI_PSEUDO);
+    }
+}
 ?>
